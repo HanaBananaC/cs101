4baafff9b464e8dc9096b85e2baf75902d1f6b04
B1329002 <hana1007c@gmail.com>
2024-12-05 14:54:54 +0800

    Update main5.c

--- EX3/main5.c
+++ EX3/main5.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main() {
+    int i = 20;
+    if (i <= 30) {
+        printf("免費\n");
+    } else if (i >= 240) {
+        printf("240元\n");
+    } else {
+        int h = ((i - 30) / 30 + 1) * 30;
+        printf("%d元\n", h);
+    }
+    return 0;
+}
