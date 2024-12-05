4805615bd0c691bb65b227bd312b6e0414f7806d
B1329002 <hana1007c@gmail.com>
2024-12-05 14:52:07 +0800

    Update main4.c

--- EX3/main4.c
+++ EX3/main4.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main() {
+    int i = 2000; // 測試年份
+    if (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) {
+        printf("閏年\n");
+    } else {
+        printf("不是閏年\n");
+    }
+    return 0;
+}
