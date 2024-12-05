1497705ea7835536f36d59ce1aadea9b809c8625
B1329002 <hana1007c@gmail.com>
2024-12-05 15:09:53 +0800

    Update main6.c

--- EX3/main6.c
+++ EX3/main6.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main() {
+    int i = 1000;
+    if (i <= 1500) {
+        printf("70元\n");
+    } else {
+        int n = i - 1500;
+        int h = (n / 100 + (n % 100 > 0 ? 1 : 0)) * 10;
+        printf("%d元\n", 70 + h);
+    }
+    return 0;
+}
