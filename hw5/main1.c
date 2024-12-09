f73c49eebc44320d6de17842c4e3605220fd73c1
B1329002 <hana1007c@gmail.com>
2024-12-09 23:27:27 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+  int n = 7;
+  int h = 1;
+  for(int i = n ; i >= 1 ; i--) {
+	  printf("%*s",i-1,"");
+    for(int j = 1 ; j <= h ; j++) {
+    		printf("%d ",h);
+		}
+  printf("\n");
+  h++;
+}
+	 
+    return 0;
+}
